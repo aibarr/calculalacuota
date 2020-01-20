e481965d2aa417cefa1e6c10430489367ca456de
aibarr <alfredo.barra@usach.cl>
2020-01-20 12:47:32 -0300

    Some first doing

---
+++ src/utils/calculations/index.js
@@ -0,0 +1,17 @@
+export const calculateAnnualInterest = (totalAmount, quotaAmount, periods) => {
+
+}
+
+export const calculateMonthlyInterest = (totalAmount, quotaAmount, periods) => {
+
+}
+
+export const calculateQuota = (totalAmount, periods, interest) => {
+
+
+}
+
+export const calculateQuotaDetailed = (totalAmount, periods, interest) => {
+
+    
+}
