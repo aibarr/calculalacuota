8ac8666767e2003392781cb62ed61a77582763d3
aibarr <alfredo.barra@usach.cl>
2020-01-24 10:34:33 -0300

    Some minimal changes

--- src/utils/calculations/index.js
+++ src/utils/calculations/index.js
@@ -7,12 +7,27 @@ export const calculateMonthlyInterest = (totalAmount, quotaAmount, periods) => {
 }
 
 export const calculateQuota = (totalAmount, periods, monthlyInterest) => {
-    const result = totalAmount * (monthlyInterest * Math.pow( 1 + monthlyInterest, periods) / (Math.pow(1 + monthlyInterest, periods) - 1))
+    const result = totalAmount * (monthlyInterest * Math.pow(1 + monthlyInterest, periods) / (Math.pow(1 + monthlyInterest, periods) - 1))
 
     return Math.round(result)
 }
 
-export const calculateQuotaDetailed = (totalAmount, periods, interest) => {
+export const calculateQuotaDetailed = (totalAmount, quota, periods, interest) => {
 
+    const result = []
+    let saldo = totalAmount
+
+    for (let i = 1; i <= periods; i++) {
+        const restInterest = saldo * interest
+        const amort = saldo - restInterest
+        result.push({
+            restInterest,
+            amort,
+            saldo,
+        })
+        saldo -= amort
+    }
+
+    return result
 
 }
